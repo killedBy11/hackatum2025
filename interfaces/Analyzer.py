17a9aaa4cd5c2d959ede65a00163b030b32c7c65
Antoniu Ficard <antoniuficard@Antonius-MacBook-Pro.local>
2025-11-22 19:53:18 +0100

    general structure

---
+++ interfaces/Analyzer.py
@@ -0,0 +1,2 @@
+# This file is licensed under the GNU General Public License v3.0 (GPLv3).
+# See the LICENSE file or https://www.gnu.org/licenses/ for details.
---
+++ main.py
@@ -0,0 +1,9 @@
+# This file is licensed under the GNU General Public License v3.0 (GPLv3).
+# See the LICENSE file or https://www.gnu.org/licenses/ for details.from sentence_transformers import SentenceTransformer
+
+def main():
+    print("Hello World")
+
+
+if __name__ == '__main__':
+    main()
